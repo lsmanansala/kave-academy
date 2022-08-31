91200f1a82a9405f0d9c56d1b2aaee220c2e468f
Ludwig Manansala <manansalaludwig@gmail.com>
2022-08-31 14:27:21 +0800

    setup for antd and react vite

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,34 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import vitePluginImp from "vite-plugin-imp";
+import { getThemeVariables } from "antd/dist/theme.js";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
-})
+  plugins: [
+    react(),
+    vitePluginImp({
+      optimize: true,
+      libList: [
+        {
+          libName: "antd",
+          libDirectory: "es",
+          style: (name) => `antd/es/${name}/style`,
+        },
+      ],
+    }),
+  ],
+  css: {
+    preprocessorOptions: {
+      less: {
+        modifyVars: {
+          ...getThemeVariables({
+            dark: true,
+          }),
+          "primary-color": "#f48c31",
+        },
+        javascriptEnabled: true,
+      },
+    },
+  },
+});
